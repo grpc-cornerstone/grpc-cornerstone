43ba4b3e9141e852491d03f4d677ff7287e36d19
Mykyta Protsenko <mykyta@protsenko.com>
2022-03-31 21:42:13 -0700

    Proto integration - do not merge (#8)
    * Fix mint-ui
    
    * Updated ui deps
    
    * Added .env file with gateway configuration
    
    * Temporary broken state - proto generated but not working
    
    * Added missing js file
    
    * Add fix for UI protogen
    
    * Added properly generated files

---
+++ mint-ui/src/App.tsx
@@ -0,0 +1,107 @@
+import Button from '@mui/material/Button';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { GatewayServiceClient } from './generated/GatewayServiceClientPb'
+import { useState } from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import './App.css';
+
+interface LeaderboardRecord {
+  currencyName: string;
+  coinsAmount: number;
+}
+
+const Home = () => {
+  const [currencyName, setCurrencyName] = useState<string>();
+  const [coinsAmount, setCoinsAmount] = useState<number>();
+
+  const client = new GatewayServiceClient('http://34.75.57.125:80');
+
+  // replace with a real call
+  const doMint = () => {
+    console.log((coinsAmount || 50) + 100);
+    setCoinsAmount((coinsAmount || 50) + 100);
+  }
+  // replace with a real call
+  if (!currencyName) {
+    setCurrencyName("Dogecoin");
+  }
+  // replace with a real call
+  if (!coinsAmount) {
+    setCoinsAmount(200);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {currencyName ? currencyName : 'Loading...'}
+        <p>{coinsAmount ? coinsAmount : ''}</p>
+        <Button variant="contained" onClick={doMint}>Mint</Button>
+      </header>
+    </div>
+  );
+}
+
+function Top5() {
+
+  // replace with a real call
+  const [leaderboard, setLeaderboard] = useState<LeaderboardRecord[]>([{ currencyName: 'Dogecoin', coinsAmount: 100500 }, { currencyName: "BitCoin", coinsAmount: 1000 }]);
+
+  const largeFont = {
+    fontSize: '15pt',
+  } as const;
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <TableContainer sx={{ width: 500 }} component={Paper}>
+          <Table aria-label="table" sx={{ fontSize: 'x-large' }}>
+            <TableHead>
+              <TableRow>
+                <TableCell aria-label='th' sx={largeFont}><b>Currency name</b></TableCell>
+                <TableCell aria-label='th' sx={largeFont} align="right"><b>Coins minted</b></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {(leaderboard || []).map((row) => (
+                <TableRow
+                  key={row.currencyName}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell scope="row" sx={largeFont}>
+                    {row.currencyName}
+                  </TableCell>
+                  <TableCell align="right" scope="row" sx={largeFont}>
+                    {row.coinsAmount}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </header>
+    </div>
+  );
+}
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="top5" element={<Top5 />} />
+      </Routes>
+    </BrowserRouter>);
+};
+
+export default App;
---
+++ mint-ui/src/generated/gateway_pb.d.ts
@@ -0,0 +1,132 @@
+import * as jspb from 'google-protobuf'
+
+
+
+export class CreateCurrencyGatewayRequest extends jspb.Message {
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): CreateCurrencyGatewayRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: CreateCurrencyGatewayRequest): CreateCurrencyGatewayRequest.AsObject;
+  static serializeBinaryToWriter(message: CreateCurrencyGatewayRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): CreateCurrencyGatewayRequest;
+  static deserializeBinaryFromReader(message: CreateCurrencyGatewayRequest, reader: jspb.BinaryReader): CreateCurrencyGatewayRequest;
+}
+
+export namespace CreateCurrencyGatewayRequest {
+  export type AsObject = {
+  }
+}
+
+export class CreateCurrencyGatewayResponse extends jspb.Message {
+  getCurrencyname(): string;
+  setCurrencyname(value: string): CreateCurrencyGatewayResponse;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): CreateCurrencyGatewayResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: CreateCurrencyGatewayResponse): CreateCurrencyGatewayResponse.AsObject;
+  static serializeBinaryToWriter(message: CreateCurrencyGatewayResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): CreateCurrencyGatewayResponse;
+  static deserializeBinaryFromReader(message: CreateCurrencyGatewayResponse, reader: jspb.BinaryReader): CreateCurrencyGatewayResponse;
+}
+
+export namespace CreateCurrencyGatewayResponse {
+  export type AsObject = {
+    currencyname: string,
+  }
+}
+
+export class MintGatewayRequest extends jspb.Message {
+  getCurrencyname(): string;
+  setCurrencyname(value: string): MintGatewayRequest;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): MintGatewayRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: MintGatewayRequest): MintGatewayRequest.AsObject;
+  static serializeBinaryToWriter(message: MintGatewayRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): MintGatewayRequest;
+  static deserializeBinaryFromReader(message: MintGatewayRequest, reader: jspb.BinaryReader): MintGatewayRequest;
+}
+
+export namespace MintGatewayRequest {
+  export type AsObject = {
+    currencyname: string,
+  }
+}
+
+export class MintGatewayResponse extends jspb.Message {
+  getNewamount(): number;
+  setNewamount(value: number): MintGatewayResponse;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): MintGatewayResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: MintGatewayResponse): MintGatewayResponse.AsObject;
+  static serializeBinaryToWriter(message: MintGatewayResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): MintGatewayResponse;
+  static deserializeBinaryFromReader(message: MintGatewayResponse, reader: jspb.BinaryReader): MintGatewayResponse;
+}
+
+export namespace MintGatewayResponse {
+  export type AsObject = {
+    newamount: number,
+  }
+}
+
+export class GetTopMintedCurrenciesGatewayRequest extends jspb.Message {
+  getMaxnumberofcurrencies(): number;
+  setMaxnumberofcurrencies(value: number): GetTopMintedCurrenciesGatewayRequest;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): GetTopMintedCurrenciesGatewayRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: GetTopMintedCurrenciesGatewayRequest): GetTopMintedCurrenciesGatewayRequest.AsObject;
+  static serializeBinaryToWriter(message: GetTopMintedCurrenciesGatewayRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): GetTopMintedCurrenciesGatewayRequest;
+  static deserializeBinaryFromReader(message: GetTopMintedCurrenciesGatewayRequest, reader: jspb.BinaryReader): GetTopMintedCurrenciesGatewayRequest;
+}
+
+export namespace GetTopMintedCurrenciesGatewayRequest {
+  export type AsObject = {
+    maxnumberofcurrencies: number,
+  }
+}
+
+export class TotalCurrencyMintedGatewayRecord extends jspb.Message {
+  getCurrencyname(): string;
+  setCurrencyname(value: string): TotalCurrencyMintedGatewayRecord;
+
+  getAmount(): number;
+  setAmount(value: number): TotalCurrencyMintedGatewayRecord;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): TotalCurrencyMintedGatewayRecord.AsObject;
+  static toObject(includeInstance: boolean, msg: TotalCurrencyMintedGatewayRecord): TotalCurrencyMintedGatewayRecord.AsObject;
+  static serializeBinaryToWriter(message: TotalCurrencyMintedGatewayRecord, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): TotalCurrencyMintedGatewayRecord;
+  static deserializeBinaryFromReader(message: TotalCurrencyMintedGatewayRecord, reader: jspb.BinaryReader): TotalCurrencyMintedGatewayRecord;
+}
+
+export namespace TotalCurrencyMintedGatewayRecord {
+  export type AsObject = {
+    currencyname: string,
+    amount: number,
+  }
+}
+
+export class GetTopMintedCurrenciesGatewayResponse extends jspb.Message {
+  getRecordsList(): Array<TotalCurrencyMintedGatewayRecord>;
+  setRecordsList(value: Array<TotalCurrencyMintedGatewayRecord>): GetTopMintedCurrenciesGatewayResponse;
+  clearRecordsList(): GetTopMintedCurrenciesGatewayResponse;
+  addRecords(value?: TotalCurrencyMintedGatewayRecord, index?: number): TotalCurrencyMintedGatewayRecord;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): GetTopMintedCurrenciesGatewayResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: GetTopMintedCurrenciesGatewayResponse): GetTopMintedCurrenciesGatewayResponse.AsObject;
+  static serializeBinaryToWriter(message: GetTopMintedCurrenciesGatewayResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): GetTopMintedCurrenciesGatewayResponse;
+  static deserializeBinaryFromReader(message: GetTopMintedCurrenciesGatewayResponse, reader: jspb.BinaryReader): GetTopMintedCurrenciesGatewayResponse;
+}
+
+export namespace GetTopMintedCurrenciesGatewayResponse {
+  export type AsObject = {
+    recordsList: Array<TotalCurrencyMintedGatewayRecord.AsObject>,
+  }
+}
+
